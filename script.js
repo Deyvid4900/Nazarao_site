4e17e1a8270c658dfbf8cb8982b93b6831dc551d
Deyvid Oliveira Silva <94657946+Deyvid4900@users.noreply.github.com>
2023-10-05 15:57:46 -0300

    muitas alteraççoes maluquetes

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-
 //efeito width no objeto selecionado
-const listas = document.querySelectorAll('.list-one a')
+const usuario = document.getElementById("usuario");
+const senhaUsuario = document.getElementById("senha");
+const btn_form = document.getElementById("btn_Acessar");
 
-function selectLista(lista){
-    const url = location.href
-    const href = lista.href
-    
+const dataDeExpiracao = new Date();
+dataDeExpiracao.setDate(dataDeExpiracao.getDate() + 7);
 
-    if(url.includes(href)){
-        lista.classList.add('ativo')
-        
-    }
+const listas = document.querySelectorAll(".list-one a");
+
+function selectLista(lista) {
+  const url = location.href;
+  const href = lista.href;
+
+  if (url.includes(href)) {
+    lista.classList.add("ativo");
+  }
 }
 
+listas.forEach(selectLista);
 
-listas.forEach(selectLista)
+const injetaDados=()=>{
+    window.location.href = "http://gruponazarao.lzt.com.br/";
+    const inputs = [...document.querySelectorAll["input"]]
+    
+
+    inputs[0].value = localStorage.getItem("username");
+    inputs[1].value = localStorage.getItem("password");
+
+}
+
+btn_form.addEventListener("click", () => {
+  const usernameValue = usuario.value;
+  const passwordValue = senhaUsuario.value;
+
+  const dataDeExpiracao = new Date();
+  dataDeExpiracao.setDate(dataDeExpiracao.getDate() + 7);
+
+  document.cookie = "username=" + usernameValue + "; expires=" + dataDeExpiracao.toUTCString() + "; path=/";
+  document.cookie = "password=" + passwordValue + "; expires=" + dataDeExpiracao.toUTCString() + "; path=/";
+
+  setTimeout(injetaDados,1000)
+});
