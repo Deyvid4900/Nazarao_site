6919b6ee7ed119f88a56a0ce9d982a7f696c304f
Deyvid Oliveira Silva <94657946+Deyvid4900@users.noreply.github.com>
2023-10-07 16:36:59 -0300

    coloquei as especificações do array da api do maps

--- api.js
+++ api.js
@@ -2,54 +2,202 @@
 // AIzaSyB46wG7bMX070nj2Avrb24R-R7F5ihAfwM
 // lat: -20.861831422377254, lng: -41.118637116638055
 // lat: -20.84100222644713, lng: -41.18210193771411
+
 const cordsPostos = [
-  { lat: -20.861831422377254, lng: -41.118637116638055 },
-  { lat: -20.84100222644713, lng: -41.18210193771411 },
-  { lat: -20.84289295305546, lng: -41.12330964952382 },
-  { lat: -20.806676356235418, lng: -41.15651244952518 },
-  { lat: -21.10271504079641, lng: -41.042290889997126 },
-  { lat: -20.678359003705445, lng: -41.349042080157936 },
-  { lat: -20.772324166996153, lng: -41.677827974441136 },
-  { lat: -20.69391837098185, lng: -41.84916749185929 },
-  { lat: -18.7463493646525, lng: -39.86130393241459 },
-  { lat: -20.763236704310643, lng: -41.59164003049385 },
-  { lat: -20.562696620673, lng: -41.82431129186433 },
-  { lat: -20.778280667176023, lng: -41.06815942253728 },
-  { lat: -20.85175905869883, lng: -41.094232307817414 },
-  { lat: -20.857203590992768, lng: -41.10262594583081 },
-  { lat: -20.814280438079546, lng: -41.15874144997447 },
-  { lat: -20.85407148702786, lng: -41.11774863233659 },
-  { lat: -20.84389240243609, lng: -41.12881863217908 },
-  { lat: -20.868121929400584, lng: -41.02589251884142 },
-  { lat: -20.832372559575184, lng: -42.15383419185384 },
-  { lat: -18.52727635721204, lng: -39.84128619640968 },
-  { lat: -21.043614786963147, lng: -40.82751636514811 },
-  { lat: -20.25150666926673, lng: -40.37321644585403 },
-  { lat: -19.27893908396705, lng: -40.088102863076855 },
+  {
+    lat: -20.861831422377254,
+    lng: -41.118637116638055,
+    nome: "Posto Avenida",
+    img:"" 
+  },
+  {
+    lat: -20.84100222644713,
+    lng: -41.18210193771411,
+    nome: "Uniposto Derivados",
+    img: " "
+  },
+  {
+    lat: -20.84289295305546,
+    lng: -41.12330964952382,
+    nome: "São judas tadeu",
+    img: " "
+  },
+  {
+    lat: -20.84888877517764,
+    lng: -41.141356476512534,
+    nome: "Posto rainha",
+    img: " "
+  },
+  {
+    lat: -20.806676356235418,
+    lng: -41.15651244952518,
+    nome: "Santa Clara",
+    img: " "
+  },
+  {
+    lat: -21.10271504079641,
+    lng: -41.042290889997126,
+    nome: "Petromax",
+    img: " "
+  },
+  {
+    lat: -20.678359003705445,
+    lng: -41.349042080157936,
+    nome: "Pedra da ema",
+    img: " "
+  },
+  {
+    lat: -20.772324166996153,
+    lng: -41.677827974441136,
+    nome: "Auto Posto Redenção",
+    img: " "
+  },
+  {
+    lat: -20.69391837098185,
+    lng: -41.84916749185929,
+    nome: "Posto Fronteira",
+    img: " "
+  },
+  {
+    lat: -18.7463493646525,
+    lng: -39.86130393241459,
+    nome: "Posto Rio Negro",
+    img: " "
+  },
+  {
+    lat: -20.763236704310643,
+    lng: -41.59164003049385,
+    nome: "Posto Celina",
+    img: " "
+  },
+  {
+    lat: -20.562696620673,
+    lng: -41.82431129186433,
+    nome: "Auto Posto pedra menina",
+    img: " "
+  },
+  {
+    lat: -20.778280667176023,
+    lng: -41.06815942253728,
+    nome: "Posto Soturno",
+    img: " "
+  },
+  {
+    lat: -20.85175905869883,
+    lng: -41.094232307817414,
+    nome: "Posto Coronel Borges",
+    img: " "
+  },
+  {
+    lat: -20.857203590992768,
+    lng: -41.10262594583081,
+    nome: "Posto Baiminas",
+    img: " "
+  },
+  {
+    lat: -20.814280438079546,
+    lng: -41.15874144997447,
+    nome: "Posto Trevo",
+    img: " "
+  },
+  {
+    lat: -20.85407148702786,
+    lng: -41.11774863233659,
+    nome: "Posto Ita",
+    img: " "
+  },
+  {
+    lat: -20.84389240243609,
+    lng: -41.12881863217908,
+    nome: "Posto alvorada",
+    img: " "
+  },
+  {
+    lat: -20.868121929400584,
+    lng: -41.02589251884142,
+    nome: "Auto posto Dantas",
+    img: " "
+  },
+  {
+    lat: -20.832372559575184,
+    lng: -42.15383419185384,
+    nome: "Posto douradense",
+    img: " "
+  },
+  {
+    lat: -18.52727635721204,
+    lng: -39.84128619640968,
+    nome: "Posto diamante negro",
+    img: " "
+  },
+  {
+    lat: -21.043614786963147,
+    lng: -40.82751636514811,
+    nome: "Posto Eclipse",
+    img: " "
+  },
+  {
+    lat: -20.25150666926673,
+    lng: -40.37321644585403,
+    nome: "Auto Posto cariacica",
+    img: " "
+  },
+  { 
+    lat: -19.27893908396705, lng: -40.088102863076855, nome: "Jequitiba" },
 ];
 
-console.log(cordsPostos);
-
-const parser = new DOMParser();
+const image = "./img-svg/svg/logo-menor.svg";
 
 async function initMap() {
   // Request needed libraries.
-  const image = "./img-svg/svg/logo-menor.svg";
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+    "marker"
+  );
   const map = new Map(document.getElementById("map"), {
+    zoom: 12,
     center: { lat: -20.861831422377254, lng: -41.118637116638055 },
-    zoom: 13,
     mapId: "4504f8b37365c3d0",
   });
 
-  cordsPostos.forEach((coordinate) => {
-    new AdvancedMarkerElement({
+  // Create an info window to share between markers.
+  const infoWindow = new InfoWindow(() => {});
+
+  // Create the markers.
+  cordsPostos.forEach((position, i) => {
+    const pin = new PinElement({});
+    const marker = new AdvancedMarkerElement({
+      position,
       map,
-      position: coordinate,
+      content: pin.element,
+    });
+    console.log(position.nome);
+
+    // Add a click listener for each marker, and set up the info window.
+    marker.addListener("click", ({ domEvent, latLng }) => {
+      const { target } = domEvent;
+
+      //   content that show when you click em pin/markes
+      const contentString =
+        '<div style="height: 100%;">' +
+        "<h1>" +
+        position.nome +
+        "</h1>" +
+        "<img src=" +
+        +' alt="" style="width: 400px;">' +
+        "</div>";
+
+      infoWindow.setContent(contentString);
+      //   infoWindow.setIcon();
+
+      infoWindow.open({
+        anchor: marker,
+        map,
+      });
     });
   });
 }
+
+initMap();
 
 window.initMap = initMap();
